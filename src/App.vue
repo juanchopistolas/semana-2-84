0f512922de364dee35295e7197476248bfafb421
afguerrerogu <afguerrerogu@unal.edu.co>
2020-12-05 18:54:02 -0500

    actualizacion con la seccion api hecha

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <carousel></carousel>
+
+    <!-- seccion api -->
+    <hr color="#ccd1d1" size=3>
+    <h5 class = "text-center text-danger font-weight-bold " >SECCIÓN DE NOTICIAS</h5>
+    <hr color="#ccd1d1" size=3>
+    <sectionapi></sectionapi>
+    <!-- fin seccion api -->
     <page-footer></page-footer>
     
   </div>
@@ -9,13 +16,14 @@
 <script>
 import Carousel from './components/carousel.vue'
 import PageFooter from './components/PageFooter.vue'
-
+import sectionapi from './components/sectionapi.vue'
 
 
 export default {
   name: 'App',
   components:{
     Carousel,
+    sectionapi,
     PageFooter
   }
 }
---
+++ src/components/sectionapi.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="container-fluid">
+      <div class="row mt-md-5 mt-sm-5 mt-xs-5">
+        <div class="col-lg-6 col-xs-12" v-for="(movie, index) of movies" :key="index">
+          <div
+            class="d-flex justify-content-center align-items-center"
+            
+          >
+            <div class="p-3">
+                <img :src="movie.urlToImage" alt="imagen noticia" class="img-fluid">
+            </div>
+
+            <div class="p-2">
+              
+                <h5 class=" font-weight-bold font-weight-light "> {{movie.title}}</h5>
+              <p class="text-secondary  text-justify ">  
+                {{movie.description}}
+              </p>
+              <h6 class="small font-italic ">{{movie.publishedAt}} </h6>
+            </div>
+          </div>
+          <div class="d-flex container-fluid justify-content-end pb-2 mt-n2">
+            <a :href="movie.url" class="btn btn-danger">new</a>
+          </div>
+          <hr color="#ccd1d1" size="3" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "sectionapi",
+  data() {
+    return {
+      movies: null,
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "http://newsapi.org/v2/top-headlines?country=co&category=science&apiKey=3366e11beeec47df8edad6be97536fa1"
+      )
+      .then((response) => {
+        this.movies = response.data.articles.slice(0, 4);
+      });
+  },
+};
+</script>
+
+<style lang="stylus" scoped></style>
