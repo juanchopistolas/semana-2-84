b9056e1453d42bcc008d17ed5e1b151bfe0a4c79
Ian2012 <crisgarta8@gmail.com>
2020-12-05 15:29:47 -0500

    Añadido archivo PageFooter

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,51 @@
+<template>
+  <footer class="page-footer bg-secondary text-white pt-4">
+    <!-- Footer Text -->
+    <div class="container-fluid text-center text-md-left">
+      <!-- Grid row -->
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+          <!-- Content -->
+          <h5 class="text-uppercase font-weight-bold">PRESENTACION</h5>
+          <p>
+            Este proyecto surge como práctica de la semana 2 del ciclo 3 de
+            Desarrollo de Software
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3" />
+
+        <!-- Grid column -->
+        <div class="col-md-6 mb-md-0 mb-3">
+          <!-- Content -->
+          <h5 class="text-uppercase font-weight-bold">TECNOLOGÍAS USADAS</h5>
+          <p>Vue.js, JavaScript, HTML5, Git, GitHub, Vue Components</p>
+          <p>
+            <a href="https://github.com/juanchopistolas/workshop1"
+              >¡Enlace al proyecto aquí!</a
+            >
+          </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+    <!-- Footer Text -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-1 bg-dark">
+      © 2020 Copyright: DAD
+    </div>
+    <!-- Copyright -->
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "PageFooter",
+};
+</script>
+
+<style scoped></style>
