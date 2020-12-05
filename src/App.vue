686734aa4b77d072a6d70ad9ca6c4945dc4d17cb
Ian2012 <crisgarta8@gmail.com>
2020-12-05 14:06:33 -0500

    Footer terminado usando Vue.js components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
+    <page-footer></page-footer>
+    
   </div>
 </template>
 
 <script>
 
+import PageFooter from './components/PageFooter.vue'
+
+
+
 export default {
   name: 'App',
+  components:{
+    PageFooter
+  }
 }
 </script>
 
